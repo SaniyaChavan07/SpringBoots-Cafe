5b80c5048a502f026b81017b2ba394b53d051feb
SaniyaChavan07 <149809216+SaniyaChavan07@users.noreply.github.com>
2023-11-06 21:51:49 +0530

    Add files via upload

---
+++ components/delicious_food_container.php
@@ -0,0 +1,314 @@
+<section id="gallery" class="padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2 class="heading ">Delicious &nbsp; Food</h2>
+                <hr class="heading_space">
+                <div class="work-filter">
+                    <ul class="text-center">
+                        <li><a href="javascript:;" data-filter="all" class="active filter">All Items</a></li>
+                        <li><a href="javascript:;" data-filter=".bites" class="filter">Mini Bites</a></li>
+                        <li><a href="javascript:;" data-filter=".desserts" class="filter">Desserts</a></li>
+                        <li><a href="javascript:;" data-filter=".hbevs" class="filter">Hot Beverages</a></li>
+                        <li><a href="javascript:;" data-filter=".cbevs" class="filter">Cold Beverages</a></li>
+                        <li><a href="javascript:;" data-filter=".appz" class="filter">Appetizers</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="zerogrid">
+                <div class="wrap-container">
+                    <div class="wrap-content clearfix home-gallery">
+                        <div class="col-1-4 mix work-item bites">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/minibites/mb1.jpg" alt="cook" />
+                                        
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/minibites/mb1.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item bites">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/minibites/mb2.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/minibites/mb2.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-1-4 mix work-item bites">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/minibites/mb4.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/minibites/mb4.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-1-4 mix work-item bites">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/minibites/mb6.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/minibites/mb6.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item bites">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/minibites/mb7.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/minibites/mb7.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-1-4 mix work-item desserts">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/desserts/d1.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/desserts/d1.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item desserts">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/desserts/d2.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/desserts/d2.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item desserts">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/desserts/d6.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/desserts/d6.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item desserts">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/desserts/d7.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/desserts/d7.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-1-4 mix work-item hbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/hotbeverages/hb1.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/hotbeverages/hb1.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item hbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/hotbeverages/hb4.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/hotbeverages/hb4.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item hbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/hotbeverages/hb7.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/hotbeverages/hb7.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item hbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/hotbeverages/hb8.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/hotbeverages/hb8.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item cbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/coldbeverages/cb1.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/coldbeverages/cb1.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item cbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/coldbeverages/cb7.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/coldbeverages/cb7.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item cbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/coldbeverages/cb16.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/coldbeverages/cb16.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item cbevs">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/coldbeverages/cb18.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/coldbeverages/cb18.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item appz">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/appetizers/a2.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/appetizers/a2.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item appz">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/appetizers/a6.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/appetizers/a6.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1-4 mix work-item appz">
+                            <div class="wrap-col">
+                                <div class="item-container">
+                                    <div class="image">
+                                        <img src="vendor/images/gallery/appetizers/a10.jpg" alt="cook" />
+                                        <div class="overlay">
+                                            <a class="fancybox overlay-inner"
+                                                href="vendor/images/gallery/appetizers/a10.jpg"
+                                                data-fancybox-group="gallery"><i class=" icon-eye6"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ components/footer.php
@@ -0,0 +1,5 @@
+ <footer class="footer">
+    <div class="credit"> copyright <sup>&copy;</sup> <?= date('Y'); ?> by <span>SpringBoots</span> | all rights reserved !
+   </div>
+
+ </footer>
